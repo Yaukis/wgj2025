5882cec2078d100f3fb9c4455b81c2cef9c4038a
NicolasSilveira96 <nicolasilveira.96@gmail.com>
2025-08-10 01:57:15 -0300

    - AudioManager.cs setup on FinalGame.unity - Music glitch on Hardmode Start, progressive pitch down

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -12,13 +12,42 @@ public class AudioManager : MonoSingleton<AudioManager>
     [SerializeField] private SoundID grabSFX;
     [SerializeField] private SoundID menuOpenSFX;
 
+    [SerializeField] private SoundID bkgMusic;
+    [SerializeField] private float pitchShiftStep = 0.01f; // amount to decrease pitch each interval
+    [SerializeField] private float pitchShiftInterval = 300f; // in milliseconds
+    private float _timer = 0f;
+    private bool _hardmodeStarted = false;
+    private float _currentPitch = 1f; // initial pitch
+
     private void Start()
     {
         EventBus<OnIngredientAddedToMixEvent>.AddListener(new EventBinding<OnIngredientAddedToMixEvent>(OnIngredientAddedToMix));
         EventBus<OnIngredientPickupEvent>.AddListener(new EventBinding<OnIngredientPickupEvent>(OnIngredientPickup));
         EventBus<OnRecipeBookOpenedEvent>.AddListener(new EventBinding<OnRecipeBookOpenedEvent>(OnRecipeBookOpened));
+        EventBus<OnHardmodeStartedEvent>.AddListener(new EventBinding<OnHardmodeStartedEvent>(OnHardmodeStarted));
+        EventBus<OnHardmodeFailedEvent>.AddListener(new EventBinding<OnHardmodeFailedEvent>(OnHardmodeFailed));
     }
-    
+
+    private void Update()
+    {
+
+        // each interval decrease the pitch by pitchShiftStep
+        if (_hardmodeStarted)
+        {
+            // make broaudio pitch be the same as pitchStart
+            
+            _timer += Time.deltaTime * 1000f; // Convert to milliseconds
+            if (_timer >= pitchShiftInterval)
+            {
+                _currentPitch -= pitchShiftStep;
+                BroAudio.SetPitch(bkgMusic, _currentPitch);
+                _timer = 0f;
+            }
+        
+        }
+    }
+
+
     private void OnDestroy()
     {
         EventBus<OnIngredientAddedToMixEvent>.RemoveListener(new EventBinding<OnIngredientAddedToMixEvent>(OnIngredientAddedToMix));
@@ -40,5 +69,17 @@ public class AudioManager : MonoSingleton<AudioManager>
     {
         BroAudio.Play(menuOpenSFX);
     }
-    
+
+    private void OnHardmodeStarted(OnHardmodeStartedEvent evt)
+    {
+        _hardmodeStarted = true;
+    }
+
+    private void OnHardmodeFailed(OnHardmodeFailedEvent evt)
+    {
+        _hardmodeStarted = false;
+        _currentPitch = 1f; // reset pitch to initial value
+        BroAudio.SetPitch(bkgMusic, _currentPitch);
+        _timer = 0f; // reset timer
+    }
 }
